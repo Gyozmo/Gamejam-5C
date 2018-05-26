6d5f7dd1bd637a00f89672a49f614bb7a6a0ffc6
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-05-26 17:53:04 +0200

    module1

---
+++ script.js
@@ -0,0 +1,33 @@
+var data = [
+{
+	title : "Germe",
+	image : "bleu.png" 
+	options : [
+	{
+		title : "Planter" ,
+		target : 1
+		
+	} ,
+
+	{
+		title : "Route" ,
+		target : 2
+		
+	}]
+}
+,
+
+{
+	title : "Planter" ,
+	image : "rouge.jpg"
+} ,
+
+{
+	title : "Route" ,
+	image : "vert.gif"
+
+}
+
+]
+
+
