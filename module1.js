499f1f0345968a31a61401eeb921ffadf61f33dc
Simplejul <simplejul@protonmail.com>
2018-05-26 23:40:31 +0200

    index js

--- module1.js
+++ module1.js
@@ -5,7 +5,7 @@ var data = [
     // Objet 0
     {
         title : "Germe",
-        image : "url(images/foot.png)",
+        image : "url(images/germe.jpg)",
         index : 0
         /*options : [
         {
@@ -13,29 +13,54 @@ var data = [
             target : 1
             
         } ,
-    
-        {
-            title : "Route" ,
-            target : 2
-            
-        }] */
+*/
     },
     
     //Objet 1
     {
         title : "Planter" ,
-        image : "url(images/burger.png)",
+        image : "url(images/route.jpg)",
         index : 1
     } ,
     
     //Objet 2
     {
         title : "Route" ,
-        image : "url(images/logo.png)",
+        image : "url(images/terre.jpg)",
         index : 2
+    },
+
+    //Objet 3
+    {
+        title : "Route" ,
+        image : "url(images/burger.png)",
+        index : 3
+    },
+
+
+    //Objet 4
+    {
+        title : "Route" ,
+        image : "url(images/foot.png)",
+        index : 4
+    },
+
+
+    //Objet 5
+    {
+        title : "Route" ,
+        image : "url(images/logo.png)",
+        index : 5
+    },
+
     
-    }
-    
+    //Objet 6
+    {
+        title : "Route" ,
+        image : "url(images/logo.png)",
+        index : 6
+    },
+
     ]
     
     var i=0;
@@ -53,22 +78,20 @@ var data = [
     $("#top").css("background-image",data[i+1].image);
     $("#bottom").css("background-image",data[i+2].image); */
     
-    console.log("ok");
+    console.log();
     
     $("#top").click(function(){
-        console.log("ok");
+        i = i + 4
         
-        console.log($(this));
-        
-        /*if ($(data[this].index) %2 == 0) {
-            $("#centre").css("background-image",data[i].image);
-            $("#top").css("background-image",data[i].image);
-            $("#bottom").css("background-image",data[i].image);
+        if (i%2 == 0) {
+            $("#centre").css("background-image",data[i].image).attr("data-index",data[i].index);
+            $("#top").css("background-image",data[i+1].image).attr("data-index",data[i+1].index);
+            $("#bottom").css("background-image",data[i+2].image).attr("data-index",data[i+2].index);
         }else{
-            $("#centre").css("background-image",data[i+3].image);
+            $("#centre").css("background-image",data[i+3].image).attr("data-index",data[i+3].index);
             $("#top").css("background-image","none");
             $("#bottom").css("background-image","none");
-        }*/
+        }
     
     
     });
