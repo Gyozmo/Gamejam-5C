cb6c8e09c7536ccd8bcc7a34f64746831471b8b1
Gyozmo <binantguillaume@gmail.com>
2018-05-27 09:57:40 +0200

    Final CSS transition

---
+++ module1.js
@@ -0,0 +1,113 @@
+$(document).ready(function(){
+
+var data = [
+
+    // Objet 0
+
+    {
+        image : ["url(images/patate-germe.png)","url(images/route.jpg)","url(images/ground.png)"]
+    },
+    {
+        image : ["url(images/burger.png)","url(images/foot.png)","url(images/fleur.png)"]
+    },
+    {
+        image : ["url(images/germe.jpg)","url(images/route.jpg)","url(images/terre.jpg)"]
+    },
+    {
+        image : ["url(images/germe.jpg)","url(images/route.jpg)","url(images/terre.jpg)"]
+    },
+    {
+        image : ["url(images/germe.jpg)","url(images/route.jpg)","url(images/terre.jpg)"]
+    },
+    {
+        image : ["url(images/germe.jpg)","url(images/route.jpg)","url(images/terre.jpg)"]
+    },
+    {
+        image : ["url(images/germe.jpg)","url(images/route.jpg)","url(images/terre.jpg)"]
+    },
+    {
+        image : ["url(images/germe.jpg)","url(images/route.jpg)","url(images/terre.jpg)"]
+    }
+    ]
+
+
+var end = [
+    {
+        image : ["url(images/small-logo.png)","url(images/crying-potato.png)","url(images/try-again.png)"]
+    },
+    {
+        image : ["url(images/small-logo.png)","url(images/crying-potato.png)","url(images/try-again.png)"]
+    },
+    {
+        image : ["url(images/small-logo.png)","url(images/crying-potato.png)","url(images/try-again.png)"]
+    },
+    {
+        image : ["url(images/small-logo.png)","url(images/crying-potato.png)","url(images/try-again.png)"]
+    },
+    {
+        image : ["url(images/small-logo.png)","url(images/crying-potato.png)","url(images/try-again.png)"]
+    },
+    {
+        image : ["url(images/small-logo.png)","url(images/crying-potato.png)","url(images/try-again.png)"]
+    },
+    {
+        image : ["url(images/small-logo.png)","url(images/crying-potato.png)","url(images/try-again.png)"]
+    },
+    {
+        image : ["url(images/epic-win.gif)","url(images/crying-potato.png)","url(images/try-again.png)"]
+    }
+    ]
+
+
+
+    var i=0;
+    
+    $("#centre").css("background-image",data[i].image[0]);
+    $("#top").css("background-image",data[i].image[1]);
+    $("#bottom").css("background-image",data[i].image[2]);
+
+    $("#top").click(function(){
+        if(i%2 !=0) {
+            $("#centre").css("background-image",data[i].image[0]);
+            $("#top").css("background-image",data[i].image[1]);
+            $("#bottom").css("background-image",data[i].image[2])
+                    }
+        else {
+            $("#centre").css("background-image",end[i].image[0]);
+            $("#top").css("background-image",end[i].image[1]);
+            $("#bottom").css("background-image",end[i].image[2]);
+            var btn = "<button onclick='goBack()'>Clique ici pour réessayer</button>"
+            $("#module").append(btn)
+            
+    }
+
+
+        i++;
+
+    });
+
+    $("#bottom").click(function(){
+        if(i%2 ==0) {
+        $("#centre").css("background-image",data[i].image[0]);
+        $("#top").css("background-image",data[i].image[1]);
+        $("#bottom").css("background-image",data[i].image[2])
+    }
+        else {
+            $("#centre").css("background-image",end[i].image[0]);
+            $("#top").css("background-image",end[i].image[1]);
+            $("#bottom").css("background-image",end[i].image[2])
+        }
+        i++;
+
+    });
+
+    function goBack() {
+                $("#centre").css("background-image",data[i].image[0]);
+                $("#top").css("background-image",data[i].image[1]);
+                $("#bottom").css("background-image",data[i].image[2]);
+                return null;
+    }
+
+    
+});
+    
---
+++ script.js
@@ -0,0 +1,113 @@
+
+
+
+var planter = document.querySelector(".planter");
+var bouger = document.querySelector(".bouger");
+var manger = document.querySelector(".manger");
+var fleur = document.querySelector(".fleur");
+var foot = document.querySelector(".foot");
+var burger = document.querySelector(".burger");
+var croix = document.querySelector(".croix");
+
+
+/****** Transition au hover ******/
+
+planter.addEventListener("mouseover", function() {
+	this.style.color = "#FFF";
+	fleur.style.visibility = "visible";
+	fleur.style.marginTop = "0";
+	fleur.style.transition = "ease-out 2s"
+})
+
+planter.addEventListener("mouseout", function() {
+	this.style.color = "#000";
+	fleur.style.visibility = "hidden";
+	fleur.style.marginTop = "30%";
+	fleur.style.transition = "ease-out 2s"
+})
+
+bouger.addEventListener("mouseover", function() {
+	this.style.color = "#FFF";
+	foot.style.visibility = "visible";
+	foot.style.marginTop = "0";
+	foot.style.transition = "ease-out 2s"
+})
+
+bouger.addEventListener("mouseout", function() {
+	this.style.color = "#000";
+	foot.style.visibility = "hidden";
+	foot.style.marginTop = "30%";
+	foot.style.transition = "ease-out 2s"
+})
+
+manger.addEventListener("mouseover", function() {
+	this.style.color = "#FFF";
+	burger.style.visibility = "visible";
+	burger.style.marginBottom = "0";
+	burger.style.transition = "ease-out 2s"
+})
+
+manger.addEventListener("mouseout", function() {
+	this.style.color = "#000";
+	burger.style.visibility = "hidden";
+	burger.style.marginBottom = "35%";
+	burger.style.transition = "ease-out 2s"
+})
+
+/******** Aggrandir la div au click *********/
+
+planter.addEventListener("click", function() {
+    this.style.transform = "scale(3.4, 2.5)";
+    this.style.transformOrigin = "bottom left";
+    this.style.zIndex = "4";
+    this.style.transition = "all 2s";
+    setTimeout(function() {
+    	croix.style.display = "block";
+    }, 2000)
+})
+
+bouger.addEventListener("click", function() {
+    this.style.transform = "scale(3.4, 2.5)";
+    this.style.transformOrigin = "bottom right";
+    this.style.zIndex = "4";
+    this.style.transition = "all 2s";
+    setTimeout(function() {
+    	croix.style.display = "block";
+    }, 2000)
+})
+
+manger.addEventListener("click", function() {
+    this.style.transform = "scale(3.4, 2.5)";
+    this.style.transformOrigin = "top";
+    this.style.zIndex = "4";
+    this.style.transition = "all 2s";
+    setTimeout(function() {
+    	croix.style.display = "block";
+    }, 2000)
+})
+
+
+/******* Réduire la div au click *********/
+
+croix.addEventListener("click", function() {
+	planter.style.transform = "scale(1, 1)";
+    planter.style.transformOrigin = "bottom left";
+    planter.style.zIndex = "2";
+    planter.style.transition = "all 2s";
+    this.style.display = "none";
+})
+
+croix.addEventListener("click", function() {
+	bouger.style.transform = "scale(1, 1)";
+    bouger.style.transformOrigin = "bottom left";
+    bouger.style.zIndex = "2";
+    bouger.style.transition = "all 2s";
+    this.style.display = "none";
+})
+
+croix.addEventListener("click", function() {
+	manger.style.transform = "scale(1, 1)";
+    manger.style.zIndex = "3";
+    manger.style.transition = "all 2s";
+    this.style.display = "none";
+})
