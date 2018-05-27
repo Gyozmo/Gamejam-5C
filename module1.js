7aff727f52c95cf02652fbb929b82d5ee34a86ea
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-05-27 12:26:25 +0200

    ok

--- module1.js
+++ module1.js
@@ -1,61 +1,74 @@
 $(document).ready(function(){
 
     var data = [
-
-    {
-        image : ["url(images/patate-germe.png)","url(images/chat.jpeg)","url(images/ground.png)"]
-    },
-    {
-        image : ["url(images/plantpotato.jpg)","url(images/plantadult.jpg)","url(images/faucille.jpg)"]
-    },
-    {
-        image : ["url(images/potatoes.gif)","url(images/epic-win.gif)","url(images/animepotato.gif)"]
-    },
-    {
-        image : ["url(images/burger.png)","url(images/route.jpg)","url(images/ground.png)"]
-    },
-    {
-        image : ["url(images/patate-germe.png)","url(images/route.jpg)","url(images/ground.png)"]
-    },
-    {
-        image : ["url(images/burger.png)","url(images/route.jpg)","url(images/ground.png)"]
-    },
-    {
-        image : ["url(images/patate-germe.png)","url(images/route.jpg)","url(images/ground.png)"]
-    },
-    {
-        image : ["url(images/epic-win.gif)","url(images/crying-potato.png)","url(images/try-again.png)"]
-    }
+        {image : [
+            "url(images/patate-germe.png)",
+            "url(images/chat.jpeg)",
+            "url(images/ground.png)"]},
+        {image : [
+            "url(images/plantpotato.jpg)",
+            "url(images/plantadult.jpg)",
+            "url(images/faucille.jpg)"]},
+        {image : [
+            "url(images/potatoes.gif)",
+            "url(images/epic-win.gif)",
+            "url(images/animepotato.gif)"]},
+        {image : [
+            "url(images/burger.png)",
+            "url(images/route.jpg)",
+            "url(images/ground.png)"]},
+        {image : [
+            "url(images/patate-germe.png)",
+            "url(images/route.jpg)",
+            "url(images/ground.png)"]},
+        {image : [
+            "url(images/burger.png)",
+            "url(images/route.jpg)",
+            "url(images/ground.png)"]},
+        {image : [
+            "url(images/patate-germe.png)",
+            "url(images/route.jpg)",
+            "url(images/ground.png)"]},
+        {image : [
+            "url(images/epic-win.gif)",
+            "url(images/crying-potato.png)",
+            "url(images/try-again.png)"]}
     ]
-
 
     var end = [
-    {
-        image : ["url(images/chatpotato.gif)","url(images/crying-potato.png)","url(images/try-again.png)"]
-    },
-    {
-        image : ["url(images/small-logo.png)","url(images/crying-potato.png)","url(images/try-again.png)"]
-    },
-    {
-        image : ["url(images/small-logo.png)","url(images/crying-potato.png)","url(images/try-again.png)"]
-    },
-    {
-        image : ["url(images/small-logo.png)","url(images/crying-potato.png)","url(images/try-again.png)"]
-    },
-    {
-        image : ["url(images/small-logo.png)","url(images/crying-potato.png)","url(images/try-again.png)"]
-    },
-    {
-        image : ["url(images/small-logo.png)","url(images/crying-potato.png)","url(images/try-again.png)"]
-    },
-    {
-        image : ["url(images/small-logo.png)","url(images/crying-potato.png)","url(images/try-again.png)"]
-    },
-    {
-        image : ["url(images/epic-win.gif)","url(images/crying-potato.png)","url(images/try-again.png)"]
-    }
+        {image : [
+            "url(images/chatpotato.gif)",
+            "url(images/crying-potato.png)",
+            "url(images/try-again.png)"]},
+        {image : [
+            "url(images/small-logo.png)",
+            "url(images/crying-potato.png)",
+            "url(images/try-again.png)"]},
+        {image : [
+            "url(images/small-logo.png)",
+            "url(images/crying-potato.png)",
+            "url(images/try-again.png)"]},
+        {image : [
+            "url(images/small-logo.png)",
+            "url(images/crying-potato.png)",
+            "url(images/try-again.png)"]},
+        {image : [
+            "url(images/small-logo.png)",
+            "url(images/crying-potato.png)",
+            "url(images/try-again.png)"]},
+        {image : [
+            "url(images/small-logo.png)",
+            "url(images/crying-potato.png)",
+            "url(images/try-again.png)"]},
+        {image : [
+            "url(images/small-logo.png)",
+            "url(images/crying-potato.png)",
+            "url(images/try-again.png)"]},
+        {image : [
+            "url(images/epic-win.gif)",
+            "url(images/crying-potato.png)",
+            "url(images/try-again.png)"]}
     ]
-
 
     $("#restart").click(function(){
         i = 0;
@@ -63,9 +76,6 @@ $(document).ready(function(){
         $("#top").css("background-image",data[i].image[1]);
         $("#bottom").css("background-image",data[i].image[2]);
     });
-
-    
-
 
     var i = 0;
     
@@ -79,15 +89,11 @@ $(document).ready(function(){
             $("#centre").css("background-image",data[i].image[0]);
             $("#top").css("background-image",data[i].image[1]);
             $("#bottom").css("background-image",data[i].image[2]);
-        }
-        else {
+        } else {
             $("#centre").css("background-image",end[i].image[0]);
             $("#top").css("background-image",end[i].image[1]);
             $("#bottom").css("background-image",end[i].image[2]);
-
-            
         }
-
     });
 
     $("#bottom").click(function(){
@@ -96,16 +102,17 @@ $(document).ready(function(){
             $("#centre").css("background-image",data[i].image[0]);
             $("#top").css("background-image",data[i].image[1]);
             $("#bottom").css("background-image",data[i].image[2]);
-        }
-        else {
+        } else {
             $("#centre").css("background-image",end[i].image[0]);
             $("#top").css("background-image",end[i].image[1]);
             $("#bottom").css("background-image",end[i].image[2]);
         }
-
     });
 
+    var croix2 = document.querySelector(".croix2");
+    croix2.addEventListener("click", function() {
+       location.href = "https://gyozmo.github.io/Gamejam-5C/";
+    })
 
-    
 });
     
