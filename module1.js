98429e3bc483f87ef0cd1b3e8110d87ff11c4870
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-05-27 10:44:01 +0200

    module#1

--- module1.js
+++ module1.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){
 
-var data = [
+    var data = [
 
     {
         image : ["url(images/patate-germe.png)","url(images/route.jpg)","url(images/ground.png)"]
@@ -9,27 +9,27 @@ var data = [
         image : ["url(images/burger.png)","url(images/foot.png)","url(images/ground.png)"]
     },
     {
-        image : ["url(images/germe.jpg)","url(images/route.jpg)","url(images/ground.png)"]
+        image : ["url(images/patate-germe.png)","url(images/route.jpg)","url(images/ground.png)"]
     },
     {
-        image : ["url(images/germe.jpg)","url(images/route.jpg)","url(images/ground.png)"]
+        image : ["url(images/burger.png)","url(images/route.jpg)","url(images/ground.png)"]
     },
     {
-        image : ["url(images/germe.jpg)","url(images/route.jpg)","url(images/ground.png)"]
+        image : ["url(images/patate-germe.png)","url(images/route.jpg)","url(images/ground.png)"]
     },
     {
-        image : ["url(images/germe.jpg)","url(images/route.jpg)","url(images/ground.png)"]
+        image : ["url(images/burger.png)","url(images/route.jpg)","url(images/ground.png)"]
     },
     {
-        image : ["url(images/germe.jpg)","url(images/route.jpg)","url(images/ground.png)"]
+        image : ["url(images/patate-germe.png)","url(images/route.jpg)","url(images/ground.png)"]
     },
     {
-        image : ["url(images/germe.jpg)","url(images/route.jpg)","url(images/ground.png)"]
+        image : ["url(images/epic-win.gif)","url(images/crying-potato.png)","url(images/try-again.png)"]
     }
     ]
 
 
-var end = [
+    var end = [
     {
         image : ["url(images/small-logo.png)","url(images/crying-potato.png)","url(images/try-again.png)"]
     },
@@ -57,6 +57,17 @@ var end = [
     ]
 
 
+    $("#restart").click(function(){
+        i=0;
+        $("#centre").css("background-image",data[i].image[0]);
+        $("#top").css("background-image",data[i].image[1]);
+        $("#bottom").css("background-image",data[i].image[2]);
+        $("#restart").prop("disabled",true);
+    });
+
+    $("#restart").prop("disabled",true);
+    
+
 
     var i=0;
     
@@ -65,48 +76,37 @@ var end = [
     $("#bottom").css("background-image",data[i].image[2]);
 
     $("#top").click(function(){
+        console.log(i)
         if(i%2 !=0) {
             i++;
             $("#centre").css("background-image",data[i].image[0]);
             $("#top").css("background-image",data[i].image[1]);
             $("#bottom").css("background-image",data[i].image[2]);
-                    }
+        }
         else {
             $("#centre").css("background-image",end[i].image[0]);
             $("#top").css("background-image",end[i].image[1]);
             $("#bottom").css("background-image",end[i].image[2]);
-            $("#bottom").click(function(){
-                i=0;
-                $("#centre").css("background-image",data[i].image[0]);
-                $("#top").css("background-image",data[i].image[1]);
-                $("#bottom").css("background-image",data[i].image[2]);
-
-            });
+            $("#restart").prop("disabled",false);
 
             
-    }
-
+        }
 
     });
 
     $("#bottom").click(function(){
+        console.log(i)
         if(i%2 ==0) {
-        i++;
-        $("#centre").css("background-image",data[i].image[0]);
-        $("#top").css("background-image",data[i].image[1]);
-        $("#bottom").css("background-image",data[i].image[2]);
-    }
+            i++;
+            $("#centre").css("background-image",data[i].image[0]);
+            $("#top").css("background-image",data[i].image[1]);
+            $("#bottom").css("background-image",data[i].image[2]);
+        }
         else {
             $("#centre").css("background-image",end[i].image[0]);
             $("#top").css("background-image",end[i].image[1]);
             $("#bottom").css("background-image",end[i].image[2]);
-            $("#bottom").click(function(){
-                i=0;
-                $("#centre").css("background-image",data[i].image[0]);
-                $("#top").css("background-image",data[i].image[1]);
-                $("#bottom").css("background-image",data[i].image[2]);
-
-            });
+            $("#restart").prop("disabled",false);
         }
 
     });
