7f9defd3c365e768ca6f8d81843fd4b035c61fc1
Simon Darmandieu <simon.darmandieu@gmail.com>
2018-05-27 09:44:36 +0200

    module#1

--- module1.js
+++ module1.js
@@ -2,31 +2,29 @@ $(document).ready(function(){
 
 var data = [
 
-    // Objet 0
-
     {
         image : ["url(images/patate-germe.png)","url(images/route.jpg)","url(images/ground.png)"]
     },
     {
-        image : ["url(images/burger.png)","url(images/foot.png)","url(images/fleur.png)"]
+        image : ["url(images/burger.png)","url(images/foot.png)","url(images/ground.png)"]
     },
     {
-        image : ["url(images/germe.jpg)","url(images/route.jpg)","url(images/terre.jpg)"]
+        image : ["url(images/germe.jpg)","url(images/route.jpg)","url(images/ground.png)"]
     },
     {
-        image : ["url(images/germe.jpg)","url(images/route.jpg)","url(images/terre.jpg)"]
+        image : ["url(images/germe.jpg)","url(images/route.jpg)","url(images/ground.png)"]
     },
     {
-        image : ["url(images/germe.jpg)","url(images/route.jpg)","url(images/terre.jpg)"]
+        image : ["url(images/germe.jpg)","url(images/route.jpg)","url(images/ground.png)"]
     },
     {
-        image : ["url(images/germe.jpg)","url(images/route.jpg)","url(images/terre.jpg)"]
+        image : ["url(images/germe.jpg)","url(images/route.jpg)","url(images/ground.png)"]
     },
     {
-        image : ["url(images/germe.jpg)","url(images/route.jpg)","url(images/terre.jpg)"]
+        image : ["url(images/germe.jpg)","url(images/route.jpg)","url(images/ground.png)"]
     },
     {
-        image : ["url(images/germe.jpg)","url(images/route.jpg)","url(images/terre.jpg)"]
+        image : ["url(images/germe.jpg)","url(images/route.jpg)","url(images/ground.png)"]
     }
     ]
 
@@ -68,45 +66,50 @@ var end = [
 
     $("#top").click(function(){
         if(i%2 !=0) {
+            i++;
             $("#centre").css("background-image",data[i].image[0]);
             $("#top").css("background-image",data[i].image[1]);
-            $("#bottom").css("background-image",data[i].image[2])
+            $("#bottom").css("background-image",data[i].image[2]);
                     }
         else {
             $("#centre").css("background-image",end[i].image[0]);
             $("#top").css("background-image",end[i].image[1]);
             $("#bottom").css("background-image",end[i].image[2]);
-            var btn = "<button onclick='goBack()'>Clique ici pour réessayer</button>"
-            $("#module").append(btn)
+            $("#bottom").click(function(){
+                i=0;
+                $("#centre").css("background-image",data[i].image[0]);
+                $("#top").css("background-image",data[i].image[1]);
+                $("#bottom").css("background-image",data[i].image[2]);
+
+            });
             
     }
 
-
-        i++;
 
     });
 
     $("#bottom").click(function(){
         if(i%2 ==0) {
+        i++;
         $("#centre").css("background-image",data[i].image[0]);
         $("#top").css("background-image",data[i].image[1]);
-        $("#bottom").css("background-image",data[i].image[2])
+        $("#bottom").css("background-image",data[i].image[2]);
     }
         else {
             $("#centre").css("background-image",end[i].image[0]);
             $("#top").css("background-image",end[i].image[1]);
-            $("#bottom").css("background-image",end[i].image[2])
-        }
-        i++;
-
-    });
-
-    function goBack() {
+            $("#bottom").css("background-image",end[i].image[2]);
+            $("#bottom").click(function(){
+                i=0;
                 $("#centre").css("background-image",data[i].image[0]);
                 $("#top").css("background-image",data[i].image[1]);
                 $("#bottom").css("background-image",data[i].image[2]);
-                return null;
-    }
+
+            });
+        }
+
+    });
+
 
     
 });
